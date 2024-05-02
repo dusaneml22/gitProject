963db5f70529ab1c1971211c998b4bdafe1bd3f2
Mayur Dusane <dusaneml22@gmail.com>
2024-05-02 16:18:03 +0530

    Added Difference file and made changes in syso of Addition file

--- Difference.java
+++ Difference.java
@@ -1,11 +1,10 @@
 package gitProject;
 
-public class Addition {
+public class Difference {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Addition1 is done.");
-		
+		System.out.println("Difference is done...");
 
 	}
 
