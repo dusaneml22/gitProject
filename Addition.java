d2616c14c9deab81be3896db4a6bca4975088cd1
Mayur Dusane <dusaneml22@gmail.com>
2024-05-02 16:03:12 +0530

    Addition and Multiple files are added...

---
+++ Addition.java
@@ -0,0 +1,12 @@
+package gitProject;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Addition is done.");
+		
+
+	}
+
+}
---
+++ Multiple.java
@@ -0,0 +1,11 @@
+package gitProject;
+
+public class Multiple {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("multiplication is done.");
+
+	}
+
+}
